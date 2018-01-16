7fe3217dc732ab459ed646b6065bb4527f145361
Beagbarreto <beagbarreto09@gmail.com>
2018-01-15 20:49:49 -0600

    complete tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,50 +2,25 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
   newTree.children = [];
-  newTree.parent = null;
  _.extend(newTree, treeMethods)
   return newTree;
 };
 
-var treeMethods = {
+var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-  var child = Tree(value);
-    child.parent = something;
-    if(this.children === []){
-      this.children = [child];
-    } else {
-      this.children.push(child);
-    }
-  };
-
-treeMethods.contains = function(target) {
-
+  var newChild = new Tree(value);
+  this.children.push(newChild);
 };
 
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
-/*var Tree = function(value) {
-
-
- newTree.addChild = function(value) {
-   var newChild = Tree(value);
-   newTree.children.push(newChild);
- };
- newTree.contains = function(target) {
-   if (newTree.value === target) {
-     return true;
+treeMethods.contains = function(target) {
+  if(this.value === target){
+    return true;
+  }
+   for (var i = 0; i < this.children.length; i++){
+    if(this.children[i].contains(target)){
+      return true;
    }
-   if (newTree.children.length) {
-     for (var i = 0; i < newTree.children.length; i++) {
-       if (newTree.children[i].contains(target)) {
-         return true;
-       }
-     }
-   }
-   return false;
- };
- return newTree;
-};*/
+  }
+  return false;
+};
